a2f8b4bd774c91f68619e083082eb5e369f572b6
DanielRTato <drodrigueztato@danielcastelao.org>
2024-10-27 12:28:42 +0100

    Todos

---
+++ Boletin_2/Boletin_2/Ej2.java
@@ -0,0 +1,13 @@
+public class Ej2 {
+    public static void main(String[] args) {
+
+        int lado = 3;
+        int area = lado * lado;
+        System.out.println("El area es: " + area);
+
+
+
+
+
+    }
+}
