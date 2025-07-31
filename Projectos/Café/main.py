8b4c796a09822f99d3f93e2fe87ec7be608587d6
DanielRTato <drodrigueztato@danielcastelao.org>
2025-07-31 02:31:44 +0200

    Add main loop for coffee machine operations

--- Projectos/Café/main.py
+++ Projectos/Café/main.py
@@ -1,3 +1,25 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+machine_on = True
+
+while machine_on:
+    options = menu.get_items()
+    order = input(f"What do you want? {options}")
+
+    if order == "off":
+        machine_on = False
+
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink):
+           if print(money_machine.make_payment(drink.cost)):
+               coffee_maker.make_coffee(drink)
