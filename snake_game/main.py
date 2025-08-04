893299b454f94a2b112e432b9a1beb35ae404d53
DanielRTato <drodrigueztato@danielcastelao.org>
2025-08-04 19:55:33 +0200

    basic structur an screen class

---
+++ snake_game/main.py
@@ -0,0 +1,20 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My Scnake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
