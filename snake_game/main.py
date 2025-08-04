42eb9037486eb42df48ad6b2d44acf66e0e60444
DanielRTato <drodrigueztato@danielcastelao.org>
2025-08-04 23:33:19 +0200

    fix title typo and initialize snake segments

--- snake_game/main.py
+++ snake_game/main.py
@@ -3,9 +3,22 @@ from turtle import Screen, Turtle
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
-screen.title("My Scnake Game")
+screen.title("My Snake Game")
 
+starting_positiong = [(0,0), (-20,0), (-40,0)]
+segments = []
 
+for position in starting_positiong:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
