aa7e17049cb809c754870953bcd7c844d2721d7b
DanielRTato <drodrigueztato@danielcastelao.org>
2025-08-06 23:36:47 +0200

    add food class and integrate food spawning in main game loop

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(600, 600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
